3b73ac60a6003e54fa1177867f44f2df7b91fb47
Nozima <nozimaqodirova07@gmail.com>
2025-03-17 01:09:10 +0500

    done

--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -11,7 +11,7 @@ const Destination = () => {
             <div className="w-[380px] h-[363px] rounded-lg bg-white drop-shadow-md p-2 flex flex-col">
                 <img src={img1} alt="" />
                 <h1 className="pt-4 pb-10">Taman Nasional Komodo</h1>
-                <div className="flex justify-between hover:pb-5 hover:duration-500 ease-in-out">
+                <div className="flex justify-between hover:scale:30% 60% hover:duration-500 ease-in-out">
                     <p className="text-[16px] font-bold text-[#295943]">Rp. 7,5jt</p>
                     <div className="flex gap-2 items-center">
                         <IoPaperPlaneSharp/>
